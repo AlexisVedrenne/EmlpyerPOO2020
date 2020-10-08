3d2c06bb2489bbfc341d0dd37b5e18a20808e4d8
alexisvedrenne482@gmail.com <alexisvedrenne482@gmail.com>
2020-10-08 10:42:52 +0200

    Ajout des accesseur et du muttateur dans la Classe Employe

--- Classes/Employe.php
+++ Classes/Employe.php
@@ -1,18 +1,43 @@
 <?php
 
-Class Employe{
-protected int $numero;
-protected string $nom;
-protected string $prenom;
-protected DateTime $dateDeNaissance;
-protected float $salaireM;
+Class Employe {
 
-public function __construct(int $num , string $pNom , string $pPrenom , DateTime $pddn , float $pSalaire){
-    $this->numero = $num;
-    $this->nom=$pNom;
-    $this->prenom=$pPrenom;
-    $this->dateDeNaissance=$pddn;
-    $this->salaireM=$pSalaire;
-}
+    protected int $numero;
+    protected string $nom;
+    protected string $prenom;
+    protected DateTime $dateDeNaissance;
+    protected float $salaireM;
+
+    public function __construct(int $num, string $pNom, string $pPrenom, DateTime $pddn, float $pSalaire) {
+        $this->numero = $num;
+        $this->nom = $pNom;
+        $this->prenom = $pPrenom;
+        $this->dateDeNaissance = $pddn;
+        $this->salaireM = $pSalaire;
+    }
+
+    public function getNumero(): int {
+        return $this->numero;
+    }
+
+    public function getNom(): string {
+        return $this->nom;
+    }
+
+    public function getPrenom(): string {
+        return $this->prenom;
+    }
+
+    public function getDateDeNaissance(): DateTime {
+        return $this->dateDeNaissance;
+    }
+
+    public function getSalaireM(): float {
+        return $this->salaireM;
+    }
+
+    function setSalaireM(float $salaireM): void {
+        $this->salaireM = $salaireM;
+    }
 
 }
